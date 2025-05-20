a8710ac63461a5249bb5f3a2c2274f2705a6f5fd
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-20 12:10:59 +0430

    l

--- poblic/animation.js
+++ poblic/animation.js
@@ -1,10 +1,12 @@
 const clickMe = document.getElementById("clickme");
+const body = document.querySelector("body");
 const flex = document.getElementById("flex");
 const hidden = document.getElementById("hidden");
 clickMe.addEventListener("click", () => {
   flex.classList.remove("hidden");
   hidden.classList.add("hidden");
   clickMe.classList.add("hidden");
+  body.classList.remove("mx-auto");
 });
 flex.addEventListener("click", () => {
   flex.classList.add("hidden");
