d4f0f23713c17fe8952e226ea8dfb54a8f3ba6e8
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-20 21:53:07 +0430

    l

--- oop/oop.js
+++ oop/oop.js
@@ -2,4 +2,15 @@ const masseg = document.getElementById("masseg");
 const text = document.getElementById("text");
 const clickme = document.getElementById("clickme");
 const matn = document.getElementById("matn");
-clickme;
+clickme.addEventListener("click", () => {
+  text.select(); // اینجا اصلاح شده
+  text.setSelectionRange(0, 99999); // برای موبایل
+
+  document.execCommand("copy");
+  masseg.classList.remove("hidden");
+  masseg.classList.add("flex");
+  setInterval(() => {
+    masseg.classList.add("hidden");
+    masseg.classList.remove("flex");
+  }, 2000);
+});
