4b7cc4a2edf12e2bb8b878719cdfcbd9178409df
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-20 21:46:29 +0430

    l

---
+++ oop/oop.js
@@ -0,0 +1,5 @@
+const masseg = document.getElementById("masseg");
+const text = document.getElementById("text");
+const clickme = document.getElementById("clickme");
+const matn = document.getElementById("matn");
+clickme;
