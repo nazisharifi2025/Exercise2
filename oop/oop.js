0c3ce8a728c6089965d8dd7404df1da7d7c83932
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-21 09:44:30 +0430

    l

--- oop/oop.js
+++ oop/oop.js
@@ -3,8 +3,8 @@ const text = document.getElementById("text");
 const clickme = document.getElementById("clickme");
 const matn = document.getElementById("matn");
 clickme.addEventListener("click", () => {
-  text.select(); // اینجا اصلاح شده
-  text.setSelectionRange(0, 99999); // برای موبایل
+  // text.select(); // اینجا اصلاح شده
+  // text.setSelectionRange(0, 99999); // برای موبایل
 
   document.execCommand("copy");
   masseg.classList.remove("hidden");
@@ -13,4 +13,19 @@ clickme.addEventListener("click", () => {
     masseg.classList.add("hidden");
     masseg.classList.remove("flex");
   }, 2000);
+  text.value = "";
 });
+function jenertorpassword() {
+  const password =
+    "ABCDEFGHIJKLMNOPQWXYZabcdefghijklmnopqwxyz123456789!@#$%^&*(){}|?";
+  let gPassword = "";
+  for (let a = 0; a < 9; a++) {
+    const randomindex = Math.floor(Math.random() * password.length);
+    gPassword += password[randomindex];
+  }
+  document.getElementById("result").innerText = gPassword;
+}
+
+// navigator.clipboard.writeText("این متن کپی می‌شود")
+// .then(() => alert("کپی شد!"))
+// .catch(err => alert("خطا در کپی: " + err));
