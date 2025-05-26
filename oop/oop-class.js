0d7d3e418d907092c283b22b0219442e2c1c605d
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-26 10:17:17 +0430

    l

--- oop/oop-class.js
+++ oop/oop-class.js
@@ -1,1 +1,13 @@
 const divparint = document.getElementById("divparint");
+
+// یک کود برای ذخیره مشخصات
+const mobail = {
+  products: [
+    {
+      titel: "Apple Watch Sereis 10",
+      urlimg: "img/w1.webp",
+      Price: "Aluminum",
+      quatity: "1800",
+    },
+  ],
+};
