e05d06cd97d30b1ed15cf7a1d18ae1f24f030a56
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-26 22:27:23 +0430

    k

--- form/forme2.js
+++ form/forme2.js
@@ -6,7 +6,12 @@ const Email = document.getElementById("Email");
 const btnSave = document.getElementById("btnSave");
 
 btnSave.addEventListener("click", () => {
-  if (name2.value.lenght < 2 && name2.value.lenght > 10) {
-    console.log("hi");
+  if (name2.value.length < 2 || name2.value.length > 10) {
+    divError.textContent = "نام باید بین ۲ تا ۱۰ کاراکتر باشد";
+  } else {
+    divError.textContent = ""; // پاک‌کردن پیام خطا در صورت درست بودن
+    console.log("اطلاعات معتبر است");
   }
 });
+//   if (name2.value.lenght < 2 || name2.value.lenght > 10) {
+//     console.log("hi");
