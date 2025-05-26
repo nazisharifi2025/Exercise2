62959b4c4929119d562c5ff733bd3b6bf109ec36
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-26 22:20:37 +0430

    l

--- form/forme2.js
+++ form/forme2.js
@@ -1,6 +1,12 @@
 const divError = document.getElementById("divError");
-const name = document.getElementById("name");
+const name2 = document.getElementById("name");
 const lastName = document.getElementById("lastName");
 const number = document.getElementById("number");
 const Email = document.getElementById("Email");
 const btnSave = document.getElementById("btnSave");
+
+btnSave.addEventListener("click", () => {
+  if (name2.value.lenght < 2 && name2.value.lenght > 10) {
+    console.log("hi");
+  }
+});
