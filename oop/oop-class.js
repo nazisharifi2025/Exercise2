cbc7f50fbdbc142c246b91609763e05ddb62ea1c
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-26 10:29:29 +0430

    l

--- oop/oop-class.js
+++ oop/oop-class.js
@@ -1,5 +1,3 @@
-const divparint = document.getElementById("divparint");
-
 // یک کود برای ذخیره مشخصات
 const mobail = {
   products: [
@@ -9,5 +7,33 @@ const mobail = {
       Price: "Aluminum",
       quatity: "1800",
     },
+    {
+      titel: "Apple Watch Sereis 9",
+      urlimg: "img/w2.webp",
+      Price: "Aluminum",
+      quatity: "1800",
+    },
+    {
+      titel: "Apple Watch Sereis 8",
+      urlimg: "img/w3.webp",
+      Price: "Aluminum",
+      quatity: "1800",
+    },
   ],
+  showing() {
+    const divparint = document.getElementById("divparint");
+    this.products.forEach((mobail, index, products) => {
+      const divChilde = document.createElement("div");
+      divChilde.className("div");
+      divChilde.innerHTML = `
+        <h1>${mobail.titel}</h1>
+        <img>${mobail.urlimg}</img>
+        <h2>${mobail.Price}</h2>
+        <p class="p")>${mobail.quatity}</p>
+
+        `;
+      divparint.append(divChilde);
+    });
+  },
 };
+mobail.showing();
