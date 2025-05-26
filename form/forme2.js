11386f890e2366f69c3ae2f719ce4bac265c98de
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-26 22:07:41 +0430

    l

---
+++ form/forme2.js
@@ -0,0 +1,6 @@
+const divError = document.getElementById("divError");
+const name = document.getElementById("name");
+const lastName = document.getElementById("lastName");
+const number = document.getElementById("number");
+const Email = document.getElementById("Email");
+const btnSave = document.getElementById("btnSave");
