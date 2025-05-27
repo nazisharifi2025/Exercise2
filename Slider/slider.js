138d1c990d47abeb92dfc9b3916bb387382892e7
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-27 22:01:35 +0430

    l

--- Slider/slider.js
+++ Slider/slider.js
@@ -3,22 +3,22 @@ const slider = document.getElementById("slider");
 const items = [
   {
     Imageurl: "../img/bg2.jpg",
+    titel: "to by",
+    slidebtn: "coffeteria",
+    description:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus nemo voluptatibus sapiente illo doloremque, ",
+  },
+  {
+    Imageurl: "../img/bg1.jpg",
     titel: "Product",
     slidebtn: "coffe",
     description:
       " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus nemo voluptatibus sapiente illo doloremque, ",
   },
   {
-    Imageurl: "../img/bg2.jpg",
-    titel: "Product",
-    slidebtn: "coffe",
-    description:
-      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus nemo voluptatibus sapiente illo doloremque, ",
-  },
-  {
-    Imageurl: "../img/bg2.jpg",
-    titel: "Product",
-    slidebtn: "coffe",
+    Imageurl: "../img/bgbook.jpg",
+    titel: "Products",
+    slidebtn: "coffe2",
     description:
       " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus nemo voluptatibus sapiente illo doloremque, ",
   },
@@ -27,8 +27,9 @@ const items = [
 let current = 0;
 
 function shoingitem(index) {
-  const slide = items[current];
-  slider.classList.add("bg-[url(../img/imgs1.jpg)]");
+  slider.innerHTML = "";
+  const slide = items[index];
+  slider.style.backgroundImage = `url('${slide.Imageurl}')`;
   const h2 = document.createElement("h2");
   h2.textContent = slide.titel;
   const p = document.createElement("p");
@@ -36,11 +37,12 @@ function shoingitem(index) {
   const h1 = document.createElement("h1");
   h1.textContent = slide.slidebtn;
   slider.appendChild(h2);
-  slide.appendChild(p);
+  slider.appendChild(p);
   slider.appendChild(h1);
 }
-shoingitem(currenttime);
+
 let currenttime = 0;
+shoingitem(currenttime);
 setInterval(() => {
   currenttime = (currenttime + 1) % items.length;
   shoingitem(currenttime);
