8de925af1852fd63b2386b295e652f428d9e3d02
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-27 10:21:57 +0430

    l

--- form/forme2.js
+++ form/forme2.js
@@ -6,7 +6,7 @@ const number = document.getElementById("number");
 const Email = document.getElementById("Email");
 const btnSave = document.getElementById("btnSave");
 const divParint = document.getElementById("divParint");
-const divshoing = document.getElementById("divshing");
+const divshoing = document.getElementById("shoingdiv");
 
 btnSave.addEventListener("click", () => {
   if (
@@ -15,14 +15,25 @@ btnSave.addEventListener("click", () => {
     number.value !== "" &&
     Email.value !== ""
   ) {
-    if (name2.value.length >= 2 && name2.value.length <= 10) {
-      console.log("hi");
+    divshoing.classList.remove("hidden");
+    if (
+      name2.value.length >= 2 &&
+      name2.value.length <= 10 &&
+      name2.value !== ""
+    ) {
+      const h1 = document.createElement("h1");
+      h1.textContent = name2.value;
+      divshoing.appendChild(h1);
     }
     if (lastName.value.length >= 2 && lastName.value.length <= 10) {
-      console.log("h");
+      const h2 = document.createElement("h1");
+      h2.textContent = lastName.value;
+      divshoing.appendChild(h2);
     }
     if (number.value.length >= 2 && number.value.length <= 10) {
-      console.log("این یک عدد است");
+      const h3 = document.createElement("h1");
+      h3.textContent = number.value;
+      divshoing.appendChild(h3);
     }
     if (
       Email.value.endsWith("@gmail.com") ||
@@ -30,13 +41,39 @@ btnSave.addEventListener("click", () => {
       Email.value.endsWith("@incloud.com") ||
       Email.value.endsWith("@yahoo.com")
     ) {
-      console.log("nazisharifi@gmail.com");
+      const h4 = document.createElement("h1");
+      h4.textContent = Email.value;
+      divshoing.appendChild(h4);
+    } else {
+      Emailerror.classList.remove("hidden");
+      Email.classList.add("border");
+      Email.classList.add("border-red-500");
     }
+    name2.value = "";
+    lastName.value = "";
+    number.value = "";
+    Email.value = "";
   } else {
     divError.classList.remove("hidden");
+
+    name2.classList.add("border");
+    name2.classList.add("border-red-500");
+    lastName.classList.add("border");
+    lastName.classList.add("border-red-500");
+    number.classList.add("border");
+    number.classList.add("border-red-500");
+    Email.classList.add("border");
+    Email.classList.add("border-red-500");
+    setInterval(() => {
+      divError.classList.add("hidden");
+      name2.classList.remove("border");
+      name2.classList.remove("border-red-500");
+      lastName.classList.remove("border");
+      lastName.classList.remove("border-red-500");
+      number.classList.remove("border");
+      number.classList.remove("border-red-500");
+      Email.classList.remove("border");
+      Email.classList.remove("border-red-500");
+    }, 9000);
   }
-  name2.value = "";
-  lastName.value = "";
-  number.value = "";
-  Email.value = "";
 });
