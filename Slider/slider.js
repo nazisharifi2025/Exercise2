813c81abfd0b1bfc01fd6e65730083de4de8aa2f
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-27 21:27:48 +0430

    l

--- Slider/slider.js
+++ Slider/slider.js
@@ -7,4 +7,22 @@ const items = [
     description:
       " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus nemo voluptatibus sapiente illo doloremque, ",
   },
+  {
+    Imageurl: "../img/bg2.jpg",
+    titel: "Product",
+    description:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus nemo voluptatibus sapiente illo doloremque, ",
+  },
+  {
+    Imageurl: "../img/bg2.jpg",
+    titel: "Product",
+    description:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus nemo voluptatibus sapiente illo doloremque, ",
+  },
 ];
+
+let current = 0;
+
+function shoingitem() {
+  const slide = items[current];
+}
