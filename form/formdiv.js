6c946d1cf596c6499578455bddcf5f5566b70326
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-28 09:58:49 +0430

    l

--- form/formdiv.js
+++ form/formdiv.js
@@ -7,6 +7,7 @@ const Email = document.getElementById("Email");
 const btnSave = document.getElementById("btnSave");
 const divParint = document.getElementById("divParint");
 const divshoing = document.getElementById("shoingdiv");
+let isValid = true;
 
 btnSave.addEventListener("click", () => {
   if (
@@ -21,33 +22,21 @@ btnSave.addEventListener("click", () => {
       name2.value.length <= 10 &&
       name2.value !== ""
     ) {
-      const h1 = document.createElement("h1");
-      h1.textContent = name2.value;
-      divshoing.appendChild(h1);
-    } else {
-      name2.classList.remove("border");
-      name2.classList.remove("border-red-500");
+      name2.classList.add("border", "border-red-500");
+      isValid = false;
     }
 
     // for last name
     if (lastName.value.length >= 2 && lastName.value.length <= 10) {
-      const h2 = document.createElement("h1");
-      h2.textContent = lastName.value;
-      divshoing.appendChild(h2);
-    } else {
-      lastName.classList.remove("border");
-      lastName.classList.remove("border-red-500");
+      lastName.classList.add("border", "border-red-500");
+      isValid = false;
     }
 
     // for number
 
     if (number.value.length >= 2 && number.value.length <= 10) {
-      const h3 = document.createElement("h1");
-      h3.textContent = number.value;
-      divshoing.appendChild(h3);
-    } else {
-      number.classList.remove("border");
-      number.classList.remove("border-red-500");
+      number.classList.add("border", "border-red-500");
+      isValid = false;
     }
 
     if (
@@ -56,12 +45,8 @@ btnSave.addEventListener("click", () => {
       Email.value.endsWith("@incloud.com") ||
       Email.value.endsWith("@yahoo.com")
     ) {
-      const h4 = document.createElement("h1");
-      h4.textContent = Email.value;
-      divshoing.appendChild(h4);
-    } else {
-      Email.classList.add("border");
-      Email.classList.add("border-red-500");
+      Email.classList.add("border", "border-red-500");
+      isValid = false;
     }
     name2.value = "";
     lastName.value = "";
@@ -80,3 +65,19 @@ btnSave.addEventListener("click", () => {
     // Email.classList.add("border-red-500");
   }
 });
+
+//   const h1 = document.createElement("h1");
+//       h1.textContent = name2.value;
+//       divshoing.appendChild(h1);
+//
+//  const h2 = document.createElement("h1");
+//       h2.textContent = lastName.value;
+//       divshoing.appendChild(h2);
+//
+//  const h3 = document.createElement("h1");
+//       h3.textContent = number.value;
+//       divshoing.appendChild(h3);
+//
+// const h4 = document.createElement("h1");
+//   h4.textContent = Email.value;
+//   divshoing.appendChild(h4);
