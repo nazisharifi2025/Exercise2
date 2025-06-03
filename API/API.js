8438bfcf31592909b10f623bacdff3326162d5c0
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-03 19:49:37 +0430

    l

--- API/API.js
+++ API/API.js
@@ -1,9 +1,9 @@
-const apiKey = "pub_f6d62b45913148c3bee350a618069981";
+// const apiKey = "pub_f6d62b45913148c3bee350a618069981";
+const apiKey = "684418efa9274f3ad6491868b0271123";
 let city = "Kabul";
 const cityName = document.getElementById("cityName");
 const btn = document.getElementById("btn");
 if (cityName.value !== "") {
   city = cityName.value;
 }
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}";
+const url = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}`;
